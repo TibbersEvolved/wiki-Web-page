a93938b1d613b5c98b19fe9b56d2416b6cf244c9
TibbersEvolved <erik.olin.spanga@gmail.com>
2024-11-06 17:49:08 +0100

    Frontend initialised with Tanstack Router, Vite, TailWind, Daisy Ui

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+import plugin from 'tailwindcss'
+import daisyui from 'daisyui'
+
+export default {
+  content: ["./src/**/*.{html,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+}
+
